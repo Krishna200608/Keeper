2d55d0ca1779c1ee279855748864b361dee4d8e3
Krishna <krishnasikheriya001@gmail.com>
2025-03-29 22:33:31 +0530

    removed /Keeper

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { NotesContextProvider } from "./context/NotesContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/Keeper">
+  <BrowserRouter>
     <NotesContextProvider>
       <App />
     </NotesContextProvider>
